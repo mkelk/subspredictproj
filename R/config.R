16cf40b1f5ed34b17b4034d9f9699c05fe12e289
kwiscion <kwiscion@gmail.com>
2018-03-20 22:47:10 +0100

    moved config to separated file

---
+++ R/config.R
@@ -0,0 +1,13 @@
+knitr::opts_chunk$set(echo = TRUE, warning=F, message=F)
+
+# source files
+customers_file <- '../data/CustomersV2.export.tsv'
+subscriptions_file <- '../data/SubscriptionsV2.export.tsv'
+gdp_file <- '../data/gdp-simple.export.tsv'
+
+# time window settings
+begin_train_window <- as.Date('2017-01-01')
+begin_validation_window <- as.Date('2017-09-01')
+end_window <- as.Date('2018-01-01')
+
+last_valid_month <- '2018-03-01'
